525557f43a747c8332ba4629d34d50ad500acbb5
iki1uc <entertrainer@web.de>
2026-08-14 19:14:49 +0200

    Update coreScan.js

--- coreScan.js
+++ coreScan.js
@@ -1,1 +1,21 @@
+export function coreScan(){
+    return {
+        id: "CORE",
+        core: true,
+        fn: true,
+        stable: true,
+        ready: true,
+
+        respo360: {
+            id: "RESPO",
+            active: true,
+            level: 360,
+            pulse: Date.now()
+        },
+
+        vector: { x:0, y:0, z:0 },
+        source: "RAW",
+        stamp: Date.now()
+    };
+}
 
