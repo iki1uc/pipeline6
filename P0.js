133ef50108e0c87f84544a067cf027ed57d677e0
iki1uc <entertrainer@web.de>
2026-08-14 19:27:58 +0200

    Change id from 'P6:P0' to 'P3:P0'

--- P0.js
+++ P0.js
@@ -1,11 +1,10 @@
 export const P0 = {
     run(core){
         return {
-            id: "P6:P0",
+            id: "P3:P0",
             phase: "wake",
             source: "RAW",
             core
         };
     }
 };
-
