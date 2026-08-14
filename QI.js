71abdb36aaaa8d5fae7549c90a77f21e5c9fa393
iki1uc <entertrainer@web.de>
2026-08-14 19:14:22 +0200

    Update QI.js

--- QI.js
+++ QI.js
@@ -1,1 +1,18 @@
+export const QI = {
+    scan(obj){
+        return {
+            id: obj.id || "CORE",
+            core: !!obj.core,
+            fn: !!obj.fn,
+            stable: !!obj.stable,
+            ready: !!obj.ready,
+            respo: !!obj.respo360,
+            time: Date.now(),
+            meta: {
+                type: typeof obj,
+                keys: Object.keys(obj)
+            }
+        };
+    }
+};
 
