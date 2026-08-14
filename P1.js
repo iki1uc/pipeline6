e5ac4a161b31eb1572a021fea18fa8e5d8787a8c
iki1uc <entertrainer@web.de>
2026-08-14 19:15:13 +0200

    Add P1 module with run function

--- P1.js
+++ P1.js
@@ -1,1 +1,11 @@
+export const P1 = {
+    run(core){
+        return {
+            id: "P6:P1",
+            phase: "structure",
+            vector: { x:1, y:1, z:1 },
+            core
+        };
+    }
+};
 
