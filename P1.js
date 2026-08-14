a46bcb26882f6ff4fe111f93357c14c54107234b
iki1uc <entertrainer@web.de>
2026-08-14 19:33:37 +0200

    Update P1 object properties in P1.js

--- P1.js
+++ P1.js
@@ -1,9 +1,13 @@
 export const P1 = {
     run(core){
         return {
-            id: "P3:P1",
+            id: "P6:P1",
             phase: "structure",
-            vector: { x:1, y:1, z:1 },
+            vector: {
+                x: 1,
+                y: 0.5,
+                z: 0.2
+            },
             core
         };
     }
